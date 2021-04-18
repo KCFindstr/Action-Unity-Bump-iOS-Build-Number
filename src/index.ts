4c3bcec7e58c6b5b0941b4bcdf74c9bf19472fad
KCFindstr <792491320@qq.com>
2021-04-17 23:48:59 -0700

    Fix code and add test data

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,55 @@
 import * as core from '@actions/core';
 import * as fs from 'fs';
+import { isEqual } from 'lodash';
 
-// const PATH = ['PlayerSettings', 'buildNumber', 'iPhone'];
+const PATH = ['PlayerSettings', 'buildNumber', 'iPhone'];
 const KEY_MATCHER = /^(\s*)([a-zA-Z0-9_]+):/;
-// const VALUE_MATCHER = /^(\s*)([a-zA-Z0-9_]+):\s+(-?[0-9]+)/;
+const VALUE_MATCHER = /^(\s*[a-zA-Z0-9_]+:\s+)(-?[0-9]+)$/;
 
 function ReplaceBuildNumber(path: string) {
   const text = fs.readFileSync(path, 'utf8');
   const lines = text.split('\n');
-  // const output: string[] = [];
   const stack: string[] = [];
   for (const i in lines) {
     const line = lines[i];
-    const matches = KEY_MATCHER.exec(line);
+    let matches = KEY_MATCHER.exec(line);
     if (!matches || matches.length !== 3) {
       continue;
     }
     const level = matches[1].length >> 1;
-    if (level > stack.length + 1) {
-      core.warning(`Line ${i}: Invalid YAML structure.`);
+    if (level > stack.length) {
+      core.warning(`Line ${i + 1}: Invalid YAML structure.`);
       continue;
+    } else {
+      while (level < stack.length) {
+        stack.pop();
+      }
+      stack.push(matches[2]);
     }
-    console.log(matches[2]);
-  }
+    if (isEqual(PATH, stack)) {
+      matches = VALUE_MATCHER.exec(line);
+      core.debug(
+        `YAML path found: ${stack.join('.')} at line ${i + 1}, value = ${
+          matches[2]
+        }`,
+      );
 
-  //   core.setOutput('oldBuildNumber', value);
-  //   core.setOutput('newBuildNumber', value + 1);
+      const value = parseInt(matches[2]);
+      lines[i] = `${matches[1]}${value + 1}`;
+      const output = lines.join('\n');
+      fs.writeFileSync(path + '.out', output);
+
+      core.setOutput('oldBuildNumber', value);
+      core.setOutput('newBuildNumber', value + 1);
+
+      return;
+    }
+  }
+  throw new Error(`Unable to find path ${PATH.join('.')} in file ${path}.`);
 }
 
 try {
-  ReplaceBuildNumber('./__tests__/ProjectSettings.asset');
-  // ReplaceBuildNumber(core.getInput('ProjectSettingsPath'));
+  ReplaceBuildNumber(core.getInput('ProjectSettingsPath'));
 } catch (error) {
   console.error(error.stack);
   core.setFailed(error.message);
